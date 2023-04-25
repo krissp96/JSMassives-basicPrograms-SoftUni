611de5cbe5a2e6914c92cf9462d50443fb198ce3
Kristiyan Petrov <114005684+krissp96@users.noreply.github.com>
2023-04-25 13:30:20 +0200

    Add files via upload

--- FirstStepsInCoding/09-YardGreening.js
+++ FirstStepsInCoding/09-YardGreening.js
@@ -21,8 +21,8 @@ function yardGreening(input) {
     let discount = price * 0.18;
     let finalPrice = price - discount;
 
-    console.log(finalPrice);
-    console.log(discount);
+    console.log(`The final price is: ${finalPrice} lv.`);
+    console.log(`The discount is: ${discount} lv.`);
     
 }
 yardGreening(["550"])
